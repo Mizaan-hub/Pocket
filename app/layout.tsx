850ac9c23d1a2909e1750e9761329b02a2a7b523
Mizaan-hub <mizaanafandi@gmail.com>
2024-11-16 00:10:34 +0530

    Changed Fonts to poppins

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,25 +1,19 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+import { Poppins } from "next/font/google";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-poppins",
 });
 
 export const metadata: Metadata = {
   title: "Pocket",
-  description: "A Cloud Storage App For Everyone",
+  description: "The Only Cloud Storage Solution You Need",
   icons: {
-    icon: "/pocket_icon.png" //"/public/pocket_icon.png"
-  }
-
+    icon: "/pocket_icon.png", //"/public/pocket_icon.png"
+  },
 };
 
 export default function RootLayout({
@@ -29,9 +23,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${poppins.variable} font-poppins antialiased`}>
         {children}
       </body>
     </html>
