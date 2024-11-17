7fcfc1d642a5f6b0cad6b9c987e80152d3ebd362
Mizaan-hub <mizaanafandi@gmail.com>
2024-11-18 00:39:16 +0530

    auth layout complete

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,28 +1,51 @@
-import Image from "next/image";
 import React from "react";
+import Image from "next/image";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex min-h-screen">
-      <section className="bg-brand p-10">
-        <div>
+      <section className="hidden w-1/2 items-center justify-center bg-brand p-10 lg:flex xl:w-2/5">
+        <div className="flex max-h-[800px] max-w-[430px] flex-col justify-center space-y-12">
           {/* "//public/pocket_logo_white.png" */}
           <Image
             src="/pocket_logo_white.png"
-            alt="Pocket Logo"
+            alt="logo"
             width={224}
             height={82}
             className="h-auto"
           />
-        </div>
-        <div className="space-y-5 text-white">
-          <h1 className="h1">Manage your files in the best way</h1>
-          <p className="body-1">
-            This is a place where you can store all your documents and much more
-          </p>
+
+          <div className="space-y-5 text-white">
+            <h1 className="h1">Manage your files the best way</h1>
+            <p className="body-1">
+              This is a place where you can store all your documents.
+            </p>
+          </div>
+          {/* "//public/Illustration.png" */}
+          <Image
+            src="/Illustration.png"
+            alt="Files"
+            width={342}
+            height={342}
+            className="transition-all hover:rotate-2 hover:scale-105"
+          />
         </div>
       </section>
-      {children}
+
+      <section className="flex flex-1 flex-col items-center bg-white p-4 py-10 lg:justify-center lg:p-10 lg:py-0">
+        <div className="mb-16 lg:hidden">
+          {/* "//public/pocket_logo_brand.png" */}
+          <Image
+            src="/pocket_logo_brand.png"
+            alt="logo"
+            width={224}
+            height={82}
+            className="h-auto w-[200px] lg:w-[250px]"
+          />
+        </div>
+
+        {children}
+      </section>
     </div>
   );
 };
